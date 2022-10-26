29099a303eb8c9f5a4c812ec35b3d2ee20d3eb1d
Emil Malm <malm.emil@icloud.com>
2022-10-26 20:26:00 +0000

    Added some stuff in package.json, and made a simple express skeleton app

---
+++ index.js
@@ -0,0 +1,20 @@
+// create express server
+const express = require('express');
+const app = express();
+
+const port = 3000;
+const ip = "10.8.40.234"
+
+// create routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+// start server
+app.listen(port, () => {
+  console.log(`Example app listening at http://${ip}:${port}`);
+});
