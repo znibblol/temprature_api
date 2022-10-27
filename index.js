3367523fc8a96b3dfaadb884fb6bedeb36ccbfd5
Emil Malm <malm.emil@icloud.com>
2022-10-27 10:32:18 +0200

    Lagt till dotenv & mongoose. Lagt in lite parametrar i .env.example

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// create express server
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-const port = 3000;
-const ip = "10.8.40.234"
+const port = process.env.PORT || 3000;
+const ip = "10.8.40.234";
+
 
 // create routes
 app.get('/', (req, res) => {
